07097c1997dee4a81142d812583aa000a830246d
Gabriel Nogueira <gabrielnogueira0000@hotmail.com>
2025-12-07 17:02:58 -0300

    fix: ajusta request listagem

--- app/usecases/visitor/list_visitors_usecase.ts
+++ app/usecases/visitor/list_visitors_usecase.ts
@@ -2,7 +2,7 @@ import Visitor from '#models/visitor'
 
 export default class ListVisitorsUseCase {
   async execute(inmateId?: number, page: number = 1, limit: number = 10) {
-    const query = Visitor.query().preload('inmate').orderBy('visit_date', 'desc')
+    const query = Visitor.query().preload('inmate').orderBy('created_at', 'desc')
 
     if (inmateId) {
       query.where('inmate_id', inmateId)
